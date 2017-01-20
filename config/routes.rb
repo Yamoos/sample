3665debb62c3d2bf63e34157704f829ee385170b
Yamus <yamus.ego.ya@gmail.com>
2017-01-21 01:36:38 +0200

    finish layout and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
 	root "static_page#home"
-	get 'static_page/home'
-	get 'static_page/help'
-	get 'static_page/about'
-	get 'static_page/contact'
+	match '/signup',  to: 'users#new',            via: 'get'
+	match '/help',    to: 'static_page#help',    via: 'get'
+	match '/about',   to: 'static_page#about',   via: 'get'
+	match '/contact', to: 'static_page#contact', via: 'get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
